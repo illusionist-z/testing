d903cf7ad02f77517241ce841cf4021e44f377ee
zinmon <zinmonthet@myanmar.gnext.asia>
2015-07-13 13:22:18 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/auth/models/Auth.php
    	apps/home/views/index.phtml

--- apps/attendancelist/models/Attendances.php
+++ apps/attendancelist/models/Attendances.php
@@ -4,6 +4,7 @@ namespace workManagiment\Attendancelist\Models;
 
 use Phalcon\Mvc\Model;
 
+//use workManagiment\Auth\Models\Db\CoreMember as corememberresult;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,16 +18,19 @@ class Attendances extends Model {
      * @return type
      * @author zinmon
      */
+  
     public function gettodaylist($name) {
         $this->db = $this->getDI()->getShared("db");
         $today = date("Y:m:d");
-       
+        //$core_member=new CoreMember();
+        //$aa=new \workManagiment\Auth\Models\Db\CoreMember();
         // for search result
         if (isset($name)) {
             
             $sql = "SELECT * FROM attendances JOIN core_member ON attendances.member_id=core_member.member_id WHERE attendances.att_date='" . $today . "' and member_login_name='" . $name . "'";
             $result = $this->db->query($sql);
             $row = $result->fetchall();
+            
         } else {
             //show att today list
             $result = $this->db->query("SELECT * FROM attendances JOIN core_member ON attendances.member_id=core_member.member_id WHERE attendances.att_date='" . $today . "'");
---
+++ apps/attendancelist/models/CoreMember.php
@@ -0,0 +1,13 @@
+<?php namespace workManagiment\Attendancelist\Models;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class CoreMember extends \Library\Core\BaseModel{
+    
+    public function initialize() {
+        parent::initialize();
+    }
+}
