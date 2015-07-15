f6dd4e190c189e73eb5dfd81a328c1b089509c96
zinmon <zinmonthet@myanmar.gnext.asia>
2015-07-15 16:16:40 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/attendancelist/models/Attendances.php
    	apps/leavedays/controllers/IndexController.php
    	apps/leavedays/views/index.phtml
    	data/logs/system/system_20150715.log

--- apps/attendancelist/models/Attendances.php
+++ apps/attendancelist/models/Attendances.php
@@ -4,9 +4,9 @@ namespace workManagiment\Attendancelist\Models;
 
 use Phalcon\Mvc\Model;
 use Phalcon\Paginator\Adapter\Model as PaginatorModel;
+
 //use workManagiment\Attendancelist\Models\CoreMember as CoreMember;
 //use workManagiment\Attendancelist\Models\Attendances as Attendances;
-
 //use workManagiment\Auth\Models\Db\CoreMember as corememberresult;
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,17 +21,16 @@ class Attendances extends Model {
      * @return type
      * @author zinmon
      */
-  
     public function gettodaylist($name) {
         $this->db = $this->getDI()->getShared("db");
         $today = date("Y:m:d");
         // for search result
         if (isset($name)) {
-             $row = $this->modelsManager->createBuilder()
+            $row = $this->modelsManager->createBuilder()
                     ->columns('att_date,member_login_name,checkin_time,checkout_time,lat,lng')
                     ->from('workManagiment\Attendancelist\Models\CoreMember')
                     ->leftJoin('workManagiment\Attendancelist\Models\Attendances', 'workManagiment\Attendancelist\Models\CoreMember.member_id = workManagiment\Attendancelist\Models\Attendances.member_id ')
-                    ->where('workManagiment\Attendancelist\Models\CoreMember.member_login_name) =' . $name .' AND workManagiment\Attendancelist\Models\Attendances.att_date =' . "'$today'" )
+                    ->where('workManagiment\Attendancelist\Models\CoreMember.member_login_name) =' . $name . ' AND workManagiment\Attendancelist\Models\Attendances.att_date =' . "'$today'")
                     ->getQuery()
                     ->execute();
         } else {
@@ -40,11 +39,11 @@ class Attendances extends Model {
                     ->columns('att_date,member_login_name,checkin_time,checkout_time,lat,lng')
                     ->from('workManagiment\Attendancelist\Models\CoreMember')
                     ->leftJoin('workManagiment\Attendancelist\Models\Attendances', 'workManagiment\Attendancelist\Models\CoreMember.member_id = workManagiment\Attendancelist\Models\Attendances.member_id ')
-                    ->where(' workManagiment\Attendancelist\Models\Attendances.att_date =' . "'$today'" )
+                    ->where(' workManagiment\Attendancelist\Models\Attendances.att_date =' . "'$today'")
                     ->getQuery()
                     ->execute();
         }
-         $currentPage = (int) $_GET["page"];
+        $currentPage = (int) $_GET["page"];
         $paginator = new PaginatorModel(
                 array(
             "data" => $row,
@@ -56,7 +55,7 @@ class Attendances extends Model {
         //print_r($list);exit;
         return $list;
     }
-    
+
     /**
      * Get user name
      * @return type
@@ -69,37 +68,33 @@ class Attendances extends Model {
         $getname = $user_name->fetchall();
         return $getname;
     }
+
     /**
-    *get Attendance List By User ID 
-    *@author Su Zin Kyaw
-    */
- 
-    public function getattlist($id,$month){
-       $this->db = $this->getDI()->getShared("db");
+     * get Attendance List By User ID 
+     * @author Su Zin Kyaw
+     */
+    public function getattlist($id, $month) {
+        $this->db = $this->getDI()->getShared("db");
         $currentmth = date('m');
-     
-       if (isset($month)) {
-              $row = $this->modelsManager->createBuilder()
-                    ->columns('att_date,member_login_name,checkin_time,checkout_time,lat,lng')
-                    ->from('workManagiment\Attendancelist\Models\CoreMember')
-                    ->leftJoin('workManagiment\Attendancelist\Models\Attendances', 'workManagiment\Attendancelist\Models\CoreMember.member_id = workManagiment\Attendancelist\Models\Attendances.member_id ')
-                    ->where('MONTH(workManagiment\Attendancelist\Models\Attendances.att_date) =' . $month .' AND workManagiment\Attendancelist\Models\Attendances.member_id =' . "'$id'" )
-                    ->getQuery()
-                    ->execute();
-       
-        
-    }
-    else{
+
+        if (isset($month)) {
             $row = $this->modelsManager->createBuilder()
                     ->columns('att_date,member_login_name,checkin_time,checkout_time,lat,lng')
                     ->from('workManagiment\Attendancelist\Models\CoreMember')
                     ->leftJoin('workManagiment\Attendancelist\Models\Attendances', 'workManagiment\Attendancelist\Models\CoreMember.member_id = workManagiment\Attendancelist\Models\Attendances.member_id ')
-                    ->where('MONTH(workManagiment\Attendancelist\Models\Attendances.att_date) =' . $currentmth .' AND workManagiment\Attendancelist\Models\Attendances.member_id =' . "'$id'" )
+                    ->where('MONTH(workManagiment\Attendancelist\Models\Attendances.att_date) =' . $month . ' AND workManagiment\Attendancelist\Models\Attendances.member_id =' . "'$id'")
                     ->getQuery()
                     ->execute();
-            
-    }
-     $currentPage = (int) $_GET["page"];
+        } else {
+            $row = $this->modelsManager->createBuilder()
+                    ->columns('att_date,member_login_name,checkin_time,checkout_time,lat,lng')
+                    ->from('workManagiment\Attendancelist\Models\CoreMember')
+                    ->leftJoin('workManagiment\Attendancelist\Models\Attendances', 'workManagiment\Attendancelist\Models\CoreMember.member_id = workManagiment\Attendancelist\Models\Attendances.member_id ')
+                    ->where('MONTH(workManagiment\Attendancelist\Models\Attendances.att_date) =' . $currentmth . ' AND workManagiment\Attendancelist\Models\Attendances.member_id =' . "'$id'")
+                    ->getQuery()
+                    ->execute();
+        }
+        $currentPage = (int) $_GET["page"];
         $paginator = new PaginatorModel(
                 array(
             "data" => $row,
@@ -111,9 +106,9 @@ class Attendances extends Model {
         //print_r($list);exit;
         return $list;
     }
-    
+
     /**
-     * 
+     * Show monthly attendance list
      * @param type $year
      * @param type $month
      * @param type $username
@@ -121,60 +116,95 @@ class Attendances extends Model {
      * @author zinmon
      */
     public function showmonthlylist($year, $month, $username) {
-       
+
         $this->db = $this->getDI()->getShared("db");
         //search monthly list data
-      
-        if ($year=="" and $month=="" and $username=="") {
-            $month = date('m');
-            $result = $this->db->query("SELECT * FROM core_member JOIN attendances ON core_member.member_id=attendances.member_id WHERE MONTH(attendances.att_date)='" . $month . "'");
-            $list = $result->fetchall();
 
-        } else { 
+        if ($year == "" and $month == "" and $username == "") {
+            $month = date('m');
+//            $result = $this->db->query("SELECT * FROM core_member JOIN attendances ON core_member.member_id=attendances.member_id WHERE MONTH(attendances.att_date)='" . $month . "'");
+//            $list = $result->fetchall();
+            $results = $this->modelsManager->createBuilder()
+                    ->columns('att_date,member_login_name,checkin_time,checkout_time')
+                    ->from('workManagiment\Attendancelist\Models\CoreMember')
+                    ->leftJoin('workManagiment\Attendancelist\Models\Attendances', 'workManagiment\Attendancelist\Models\CoreMember.member_id = workManagiment\Attendancelist\Models\Attendances.member_id ')
+                    ->where('MONTH(workManagiment\Attendancelist\Models\Attendances.att_date) =' . $month)
+                    ->getQuery()
+                    ->execute();
+        } else {
             //show monthly list
             //$sql="SELECT * FROM core_member JOIN attendances ON core_member.member_id=attendances.member_id where YEAR(attendances.att_date) like " . $year . " or MONTH(attendances.att_date) like " . $month . " or member_login_name='" . $username . "'";
-            $select = "SELECT * FROM core_member JOIN attendances ON core_member.member_id=attendances.member_id ";
-            
-             $conditions=$this->setCondition($year, $month, $username);
-             $sql = $select;
-              if (count($conditions) > 0) {
-              $sql .= " WHERE " . implode(' AND ', $conditions);
-              }
-              $result = $this->db->query($sql);
-              $list = $result->fetchall();
-              
+//            $select = "SELECT * FROM core_member JOIN attendances ON core_member.member_id=attendances.member_id ";
+//            
+//             $conditions=$this->setCondition($year, $month, $username);
+//             $sql = $select;
+//              if (count($conditions) > 0) {
+//              $sql .= " WHERE " . implode(' AND ', $conditions);
+//              }
+//              $result = $this->db->query($sql);
+//              $list = $result->fetchall();
+
+            $results = $this->modelsManager->createBuilder()
+                    ->columns('att_date,member_login_name,checkin_time,checkout_time')
+                    ->from('workManagiment\Attendancelist\Models\CoreMember')
+                    ->leftJoin('workManagiment\Attendancelist\Models\Attendances', 'workManagiment\Attendancelist\Models\CoreMember.member_id = workManagiment\Attendancelist\Models\Attendances.member_id ')
+                    //if (count($conditions) > 0) 
+                    //{
+                    ->where($this->setCondition($year, $month, $username))
+
+                    // }
+                    ->getQuery()
+                    ->execute();
         }
-        
+
+        $currentPage = (int) $_GET["page"];
+        $paginator = new PaginatorModel(
+                array(
+            "data" => $results,
+            "limit" => 1,
+            "page" => $currentPage
+                )
+        );
+        $list = $paginator->getPaginate();
         //print_r($list);exit;
         return $list;
     }
 
+    /**
+     * Set Condition
+     * @param type $year
+     * @param type $month
+     * @param type $username
+     * @return string
+     * @author zinmon
+     */
     public function setCondition($year, $month, $username) {
         $conditions = array();
 
         if ($year != "") {
             //echo $year;exit;
-            $conditions[] = "YEAR(Attendances.att_date) like " . $year;
+            $conditions[] = "YEAR(workManagiment\Attendancelist\Models\Attendances.att_date) like " . $year;
         }
         if ($month != "") {
 
-            $conditions[] = "MONTH(Attendances.att_date) like " . $month;
+            $conditions[] = "MONTH(workManagiment\Attendancelist\Models\Attendances.att_date) like " . $month;
         }
         if ($username != "") {
-            $conditions[] = "member_login_name='" . $username . "'";
+            $conditions[] = "workManagiment\Attendancelist\Models\CoreMember.member_login_name='" . $username . "'";
         }
 
         //$sql = $select;
-//        if (count($conditions) > 0) {
-//            $result = implode(' AND ', $conditions);
-//            //echo $result;exit;
-//        } else {
-//            $result = $conditions;
-//        }
+        if (count($conditions) > 0) {
+            $result = implode(' AND ', $conditions);
+            //echo $result;exit;
+        } else {
+            $result = $conditions;
+        }
         //print_r($result);exit;
-        return $conditions;
+        return $result;
     }
-    public function userAction(){
+
+    public function userAction() {
         
     }
 
