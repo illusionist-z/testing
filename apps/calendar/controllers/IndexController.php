c244bea0b4afbefaccc514c660d7339d32d408d1
davijp <davidjawphan@gmail.com>
2015-07-27 14:03:58 +0630

    #1114 Calendar #Added calendar table sql -- #Added full calendar js and css  file #Changed calendar url in all index.phtml

--- apps/calendar/controllers/IndexController.php
+++ apps/calendar/controllers/IndexController.php
@@ -4,28 +4,99 @@ namespace workManagiment\Calendar\Controllers;
 
 class IndexController extends ControllerBase
 {
-    
+    public $calendar;
     public function initialize() {
-        parent::initialize();
-        
+        parent::initialize();  
+        $this->calendar = new \workManagiment\Calendar\Models\Calendar();
         $this->setCommonJsAndCss();
-        
+         $this->assets->addCss('apps/calendar/css/calendar.css');
+        $this->assets->addCss('common/css/jquery-ui.css');
+        $this->assets->addCss('common/css/style.css');  
+        $this->assets->addCss('apps/calendar/css/fullcalendar.min.css');  
+        $this->assets->addJs('apps/calendar/js/moment.min.js');
+        $this->assets->addJs('apps/calendar/js/fullcalendar.min.js');        
+        $this->assets->addJs('apps/calendar/js/calendar.js');        
     }
 
     
-    public function indexAction(){
-        //$this->assets->addCss('common/css/home/home.css');
-        
-        $user = $this->session->get('user');
-        
-        $this->view->user = $user;
+   public function indexAction() {
         
     }
-    
-    public function todaylistAction() {
-        echo "Today list";
-        exit;
+    /**
+     * @desc calendar event show
+     * @author david
+     * @since 27/7/15
+     */
+
+    public function showdataAction() {
+        $this->view->disable();        
+        $events = $this->calendar->fetch();
+        echo json_encode($events);
     }
+    /**
+     * @author   David
+     * @category create event
+     * @return   json { error message }
+     */
+    public function createAction() {
+        $this->view->disable();
+        $sdate = $this->request->get('sdate');
+        $edate = $this->request->get('edate');
+        $title = $this->request->get('title');                
+        $res= array();
+        if ($title == null) {            
+            $res['cond']=FALSE;
+            $res['res']="title not be empty";            
+        }
+        else if(strtotime($sdate)>strtotime($edate)){
+            $res['cond']=FALSE;
+            $res['date']="End date must be greater than start date";
+        }
+        else {            
+            $res['cond']=TRUE;
+            $event=$this->calendar->create_event($sdate, $edate, $title);
+            $res['res']=  $event;            
+        }
+        echo json_encode($res);
+    }
+    /**
+     * @author   David
+     * @category edit event
+     * @return   json { error message }
+     */
+    public function editAction() {
+        $this->view->disable();
+        $id = $this->request->get('id');
+        $sdate = $this->request->get('sdate');
+        $edate = $this->request->get('edate');
+        $title = $this->request->get('title');        
+         $res= array();
+        if ($title == null) {            
+            $res['cond']=FALSE;
+            $res['res']="title not be empty";            
+        }
+        else if(strtotime($sdate)>strtotime($edate)){
+            $res['cond']=FALSE;
+            $res['date']="End date must be greater than start date";
+        }
+        else {            
+            $res['cond']=TRUE;
+            $edit=$this->calendar->edit_event($sdate, $edate, $title, $id);
+            $res['res']=$edit;            
+        }
+        echo json_encode($res);       
+    }
+    /**
+     * @desc Delete event
+     * @author David
+     * @since 27/7/15
+     */
+    public function deleteAction() {
+        $this->view->disable();
+        $id = $this->request->get('data');        
+        $this->calendar->delete_event($id);
+    }
+
 
 }
 
---
+++ public/apps/calendar/js/calendar.js
@@ -0,0 +1,215 @@
+//dialog box ,event edit box   
+
+var Dialog = {
+    isClick: false,
+    isOvl: false,
+    init: function () {
+        this.isOvl = true;
+    },
+    open: function (event) {
+        if (!this.isOvl) {
+            this.init();
+        }
+        $ovl = $('#dialog');
+        $('#title_edit_event').val(event.title);
+        $('#sdate_edit_event').val(event.start.format());
+        /* event.end date is empty ,put start date */
+        if (event.end == null) {
+            $('#edate_edit_event').val(event.start.format());
+        }
+        else {
+            $('#edate_edit_event').val(event.end.format());
+        }
+        $('#sdate_edit_event').datepicker({dateFormat: 'yy-mm-dd'});
+        $('#edate_edit_event').datepicker({dateFormat: 'yy-mm-dd'});
+        $ovl.dialog({
+            autoOpen: false,
+            closeText: "",
+            height: 420,
+            width: 450,
+            modal: true
+        });
+       
+        $('#submit_edit_event').click(function () {
+             $('.err').text('');
+        $('.err-sdate').text('');
+            Dialog.edit(event.id);
+        });
+        $('#del_event').click(function () {
+            Dialog.delete(event.id);
+        });
+        $ovl.dialog("open");
+    },
+    new : function (start, end) {
+        if (!this.isOvl) {
+            this.init();
+        }
+        $dia = $('#dialog_create');
+        $sdate = $('#sdate_create_event').val(start);
+        $edate = $('#edate_create_event').val(end);
+        $title = $('#title_create_event').val();
+        $('#sdate_create_event').datepicker({dateFormat: 'yy-mm-dd'});
+        $('#edate_create_event').datepicker({dateFormat: 'yy-mm-dd'});
+        $dia.dialog({
+            autoOpen: false,
+            closeText: "",
+            height: 420,
+            width: 450,
+            modal: true
+        });
+        $('#reset_create_event').click(function () {
+            $dia.dialog("close");
+        });
+        /*for edit box value err clear */
+        $('.err').text('');
+        $('.err-sdate').text('');
+        $('#create_dialog').click(function () {
+            Dialog.create();
+        });
+        $dia.dialog("open");
+    },
+    edit: function (id) {
+        $.ajax({
+            url: "index/edit?id=" + id,
+            data: $('#edit_event').serialize(),
+            async: false,
+            dataType: 'json',
+            success: function (d) {
+                if (false == d.cond) {
+                    $('.err').text(d.res).css("color", "red");
+                    $('.err-sdate').text(d.date).css("color", "red");
+                }
+                else {
+                    $('body').load("index");
+                }
+            }
+        });
+    },
+    //drag & resize event 
+    drag: function (start, end, id, title) {
+        $.ajax({
+            url: "index/edit",
+            data: {sdate: start, edate: end, id: id, title: title},
+            async: false,
+            dataType: 'json'
+        });
+    },
+    //create new event
+    create: function () {
+        $.ajax({
+            url: "index/create",
+            data: $('#create_event').serialize(),
+            async: false,
+            dataType: "json",
+            success: function (d) {
+                if (false == d.cond) {
+                    $(".err").text(d.res).css("color", "red");
+                    $(".err-sdate").text(d.date).css("color", "red");
+                }
+                else {
+                    $('body').load("index");
+                }
+            }
+
+        });
+    },
+    delete: function (id) {
+        $.ajax({
+            url: "index/delete",
+            data: {data: id},
+            async: false,
+            dataType: 'json'
+        }).
+                done(
+                        $('body').load('index')
+                        );
+    }
+};
+$(document).ready(function () {
+
+    //get event from database
+    $.ajax({
+        url: 'index/showdata',
+        type: 'POST',
+        datyType: 'json',
+        async: false,
+        success: function (response) {
+            json_events = response;
+        }
+    });
+
+    /* initialize the calendar
+     -----------------------------------------------------------------*/
+    //Date for the calendar events (dummy data)
+//        var date = new Date();
+//        var d = date.getDate(),
+//                m = date.getMonth(),
+//                y = date.getFullYear();
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        buttonText: {
+            today: 'today',
+            month: 'month',
+            week: 'week',
+            day: 'day'
+        },
+        events: JSON.parse(json_events),
+        //Random default events
+        selectable: true,
+        resizable: true,
+        editable: true,
+        droppable: true, // this allows things to be dropped onto the calendar !!!
+        eventResize: function (event) {
+            var start = event.start.format("YYYY-MM-DD");
+            var end = event.end.format("YYYY-MM-DD");
+            var shr = event.start.format("HH:mm:ss");
+            var ehr = event.end.format("HH:mm:ss");
+            //var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");  
+            Dialog.drag(start, end, event.id, event.title);
+        },
+        eventDrop: function (event) {
+            var start = event.start.format("YYYY-MM-DD"), end;
+            if (event.end == null) {
+                end = event.start.format("YYYY-MM-DD");
+            }
+            else {
+                end = event.end.format("YYYY-MM-DD");
+            }
+            Dialog.drag(start, end, event.id, event.title);
+        },
+        select: function (start, end, allDay) {
+            var start = start.format("YYYY-MM-DD");
+            var end = end.format("YYYY-MM-DD");
+            Dialog.new(start, end);
+        },
+        eventMouseout: function (calEvent, domEvent) {
+           $('table').remove('.popup');
+        },
+        eventMouseover: function (event) {
+            var start = event.start.format(),end;           
+            if(event.end == null){
+                 end = event.start.format();
+            }else{
+                end =event.end.format();
+            }
+            
+            if ($(this).hasClass('popup')) {
+                deselect($(this));
+            } else {
+            var str = "<table style='width:500px;height:100px;background:#aaa999;' border='1px' class='popup'><thead style='background:#fff;color:#000;'><td>Event</td><td>Description</td></thead>";
+            str += "<tr><td>Title</td><td>" + event.title + "</td></tr>";
+            str += "<tr><td>Time</td><td>" + start + "  - " + end + "</td></tr></table>";                              
+               $(this).append(str);               
+            }                                    
+        },
+        eventClick: function (event) {
+            Dialog.open(event);
+            //$ovl.dialog("open");
+        }
+    });
+});
+
