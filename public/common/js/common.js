a81e70cde6ceb9a65e92d3df387915257a9610d6
davijp <davidjawphan@gmail.com>
2015-08-05 16:37:33 +0630

    #Toggle menu repair

--- public/common/js/common.js
+++ public/common/js/common.js
@@ -13,25 +13,25 @@ function geo() {
         var n = new Date();
         var offset = n.getTimezoneOffset();
         $.ajax({
-        url:  url +"?offset="+offset ,
-        type: 'GET',
-        dataType: 'json'
-    });
+            url: url + "?offset=" + offset,
+            type: 'GET',
+            dataType: 'json'
+        });
     }
 }
 function GEOprocess(position) {
     //GET geo location of user
     var url = "location_session";
-    
+
     $.ajax({
-        url:  url + "?lat=" + position.coords.latitude + "&lng=" +position.coords.longitude,
+        url: url + "?lat=" + position.coords.latitude + "&lng=" + position.coords.longitude,
         type: 'GET',
         dataType: 'json',
         success: function (d) {
-            
+
         },
-        error: function (d) {            
-            
+        error: function (d) {
+
         }
     });
 }
@@ -39,59 +39,85 @@ function GEOprocess(position) {
  * @4:00pm check absent member
  * @author David
  */
-function getAbsentMember(){
+function getAbsentMember() {
     var x = new Date();
-    var h = x.getHours();    
-    if(h==16){
-    $.ajax({
-        url:baseUri+"attendancelist/absent",
-        type:'GET',
-        success:function(d){            
-        }
-    });
+    var h = x.getHours();
+    if (h == 16) {
+        $.ajax({
+            url: baseUri + "attendancelist/absent",
+            type: 'GET',
+            success: function (d) {
+            }
+        });
     }
 }
 
-$(document).ready(function(){        
+$(document).ready(function () {
     //absent member
-    $('body').attr('onload',getAbsentMember());
+    $('body').attr('onload', getAbsentMember());
     // ここに実際の処理を記述します。
-    var logout = function(){
+    var logout = function () {
         window.location.href = baseUri + 'auth/logout';
-        
+
     };
 //   
     // ユーザーのクリックした時の動作。
-    $('#btn_logout').click(function(){        
+    $('#btn_logout').click(function () {
         alert("ログアウトしました。");
         logout();
     });
-    
-    $('#btnLogin').click(function(){        
-         var url = "location_session";
-    var n = new Date();
-    
-   
-    var offset = n.getTimezoneOffset();
-    
-    $.ajax({
-        url:  url +"?offset="+offset ,
-        type: 'GET',
-        dataType: 'json',
-        success: function (d) {
-            
-        },
-        error: function (d) {            
-            
+
+    $('#btnLogin').click(function () {
+        var url = "location_session";
+        var n = new Date();
+
+
+        var offset = n.getTimezoneOffset();
+
+        $.ajax({
+            url: url + "?offset=" + offset,
+            type: 'GET',
+            dataType: 'json',
+            success: function (d) {
+
+            },
+            error: function (d) {
+
+            }
+        });
+    });
+
+    var logout = function () {
+        window.location.href = baseUri + 'auth/logout';
+
+    };
+
+
+    $('.sidebar-toggle').click(function (e) {
+        e.stopPropagation();
+        //get collapse content selector
+        var collapse_content_selector = $(this).attr('href');
+
+        //make the collapse content to be shown or hide
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function () {
+            if ($(this).css('display') == 'none') {
+                //change the button label to be 'Show'
+                toggle_switch.html('Show');
+            } else {
+                //change the button label to be 'Hide'
+                toggle_switch.html('Hide');
+            }
+        });
+
+    });
+    //
+    $('body').click(function (e) {       
+        if (0 == $(e.target).closest('#sidepage').length) {
+            $('#sidepage').fadeOut(200);
         }
     });
-    });
-    
-      var logout = function(){
-        window.location.href = baseUri + 'auth/logout';
-        
-    };
-  
 //   
-   $('.datepicker').datepicker();        
+    $('.datepicker').datepicker();
 });
+  
