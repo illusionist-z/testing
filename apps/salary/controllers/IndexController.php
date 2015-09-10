bb48692ba8bcd0919a4f48754c264ad65ff2a496
PhoneDar <PhoneDar@192.168.123.3>
2015-09-10 09:09:57 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/calendar/controllers/IndexController.php
    	apps/calendar/views/index.phtml
    	apps/dashboard/models/Attendances.php

--- apps/salary/controllers/IndexController.php
+++ apps/salary/controllers/IndexController.php
@@ -22,7 +22,7 @@ class IndexController extends ControllerBase {
         $this->assets->addCss('common/css/dialog.css');
         $this->assets->addCss('common/css/jquery-ui.css');
         $this->assets->addCss('apps/salary/css/salary.css');        
-        $this->assets->addJs('common/js/paging.js');
+        //$this->assets->addJs('common/js/paging.js');
         $this->assets->addJs('common/js/popup.js');    //popup message
         $this->assets->addJs('apps/salary/js/salary.js');
         $this->assets->addJs('common/js/export.js');
