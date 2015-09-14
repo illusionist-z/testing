f64cabb9413954ad3b4576a764e2a360c48025b5
PhoneDar <PhoneDar@192.168.123.4>
2015-09-14 16:34:06 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/calendar/views/index.phtml

--- public/apps/salary/js/salary.js
+++ public/apps/salary/js/salary.js
@@ -1,3 +1,4 @@
+
 /**
  * @author David JP<david.gnext@gmail.com>
  * @desc   Salary Edit Dial Box
@@ -16,34 +17,39 @@ var Salary = {
         $.ajax({
            url:"editsalary?id="+d,
            type: "GET",
-           success:function(res){
+          success:function(res){
                var result = $.parseJSON(res);               
-               var data ='<form id="edit_salary" width="650px" height="500px"><input type="hidden" value='+result.data[0]['id']+ ' name="id"><table width="550px" height="300px" >';               
+               var data ='<form id="edit_salary" width="650px" height="500px"><table width="550px" height="300px" >';               
                    data +='<tr><td></td><td><b>User Name </b></td>'
-                        +'<td><input style="margin-top:10px;" type="text" value='+result.data[0]['member_id']+ ' name="uname"></td><td ></td></tr>'
+                        +'<td><input style="margin-top:10px;" type="text" value= " '+result.data[0]['member_login_name']+ ' " name="uname" disabled></td><td ></td></tr>'
                         +'<tr><td></td><td><b>Basic Salary </b></td>'
                         +'<td><input style="margin-top:10px;" type="text" value='+result.data[0]['basic_salary']+ ' name="basesalary" id="baseerr"></td></tr>'
                         +'<tr><td></td><td><b>Travel Fee </b></td>'
                         +'<td><input style="margin-top:10px;" type="text" value='+result.data[0]['travel_fee']+ ' name="travelfee" id="travelerr"></td><td style="width:55px;height:40px;"></td></tr>'
                         +'<tr><td></td><td><b>Over Time </b></td>'
                         +'<td id="overmsg"><input style="width:50px;margin-top:10px;" type="text" value="'+result.data[0]['over_time']+'" name="overtime" id="overerr"> %</td></tr>'
-                        +'<tr><td></td><td><b>SSC Emp </b></td>'
+                        +'<tr><td></td><td>SSC Emp </td>'
                         +'<td id="empmsg"><input style="width:50px;margin-top:10px;" type="text" value='+result.data[0]['ssc_emp']+' name="ssc_emp" id="emperr"> %</td><td style="width:55px;height:40px;"></td></tr>'
                         +'<tr><td></td><td><b>SSC Comp </b></td>'
-                        +'<td id="compmsg"><input style="width:50px;margin-top:10px;" type="text" value='+result.data[0]['ssc_comp']+ ' name="ssc_comp" id="comperr"> %</td></tr>'
-                        +'<tr><td></td><td><input type="hidden" value='+result.data[0]['id']+ ' name="id"></td><td style="width:55px;height:40px;"></td></tr>';
-               data += '<tr><td colspan="4" style="font-size:12px;">';
+                        +'<td id="compmsg"><input style="width:50px;margin-top:10px;" type="text" value='+result.data[0]['ssc_comp']+ ' name="ssc_comp" id="comperr"> %</td></tr>';
+                       
+               data += '<tr><td></td><td>Decut Name </td><td colspan="4" style="font-size:12px;">';
                for(var j in result.dedution){
-               var duct = Salary.Check(result.dedution[j]['deduce_name'],result.permit_dedution);
-               data +=result.dedution[j]["deduce_name"]+'<input type="checkbox" name="check_list[]" value="'+result.dedution[j]["deduce_id"]+'" '+(duct!=='undefined'?duct:"") +'>';
+               var duct = Salary.Check(result.dedution[j]['deduce_id'],result.permit_dedution);
+               data +=' <input type="checkbox" name="check_list[]" value="'+result.dedution[j]["deduce_id"]+'" '+(duct!=='undefined'?duct:"") +'> '+result.dedution[j]["deduce_name"]+'<br>';
                }
-               data +='</td></tr>';
+               data +='<br></td></tr>';
+               
+                data += '<tr><td></td><td>Allow Name </td><td colspan="4" style="font-size:12px;">';
                for(var i in result.allowance){
                var cond=Salary.Check(result.allowance[i]['allowance_name'],result.permit_allowance);
-               data +='<tr><td></td><td><input type="checkbox" name="check_allow[]" value="'+result.allowance[i]["allowance_id"]+'" '+ (cond!=='undefined'?cond:"") +'>' + result.allowance[i]["allowance_name"]+'</td></tr>';
-               }
+               data +=' <input type="checkbox" name="check_allow[]" value="'+result.allowance[i]["allowance_id"]+'" '+ (cond!=='undefined'?cond:"") +'> '+ result.allowance[i]["allowance_name"] +'<br>';
+              }
+             data +='</td></tr>';
+                data += '<tr><td></td><td><input type="hidden" value='+result.data[0]['id']+ ' name="id"></td><td style="width:55px;height:40px;"></td></tr>';
+             
                data +='<tr><td></td><td></td><td colspan="3"><a href="#" class="button" id="edit_salary_edit" >Edit</a><a href="#" class="button" id="edit_delete" >Delete</a><a href="#" class="button" id="edit_close" >Cancel</a></td></tr>';
-               data +='</table></form>';
+             data +='</table></form>';
                Salary.Dia(data);
            }
         });
@@ -70,7 +76,7 @@ var Salary = {
         $ovl.css('background','#F5F5F5');
         $ovl.dialog({
             autoOpen: false,
-            height: 500,
+            height: 600,
             async: false,
             width: 600,
             modal: true,
