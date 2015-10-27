e830b68b2d355e5becc93e67f73e3ea70319175b
zinmonthet <zinmon@192.168.179.2>
2015-10-27 15:57:54 +0630

    fix dialog translation

--- apps/calendar/controllers/IndexController.php
+++ apps/calendar/controllers/IndexController.php
@@ -97,6 +97,7 @@ class IndexController extends ControllerBase
         $edate = $this->request->get('edate');
         $title = $this->request->get('title');
         $creator_id=$this->session->user['member_id'];
+        $creator_name=$this->session->user['member_login_name'];
         $res= array();
         if ($title == null ) {
             $res['cond']=FALSE;
@@ -112,7 +113,7 @@ class IndexController extends ControllerBase
         }
         else {            
             $res['cond']=TRUE;
-            $event=$this->calendar->create_event($creator_id,$id,$sdate, $edate, $title,$uname);
+            $event=$this->calendar->create_event($creator_name,$creator_id,$id,$sdate, $edate, $title,$uname);
             $res['res']=  $event;
             $res['name']= $uname;
         }
