ae827667cd7f36d13012102c736ca32e779fdc8a
suzinkyaw <gnext.suzin@gmail.com>
2015-10-29 08:38:11 +0630

    Repair

---
+++ apps/notification/messages/en.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$messages = array(
+    "username" => "User Name",
+    "startdate" => "Start Date",
+    "enddate" =>"End Date",
+    "leavestatus" => "Leave Status",
+    "leaveday" => "Leave Days",
+    "leavecategory" => "Leave Category",
+    "leavedes" => "Leave Description",
+    
+);
+
+
