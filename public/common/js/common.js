8e559265422f45e5c976827d1b3a3c55b997b9f2
davijp <davidjawphan@gmail.com>
2015-11-02 09:00:44 +0630

    #2/11/15 -module update

--- public/common/js/common.js
+++ public/common/js/common.js
@@ -15,7 +15,7 @@ function link_height() {
     //for link border right in link page
 var link_width = $(document).outerWidth();
 var link_height = $(document).outerHeight()-($(".main-footer").outerHeight()+$(".navbar").outerHeight());
-if(link_width>745){
+if(link_width>770){
 $(".link").css({"height":link_height+"px","border-right":"1px solid #aaa","background":"#fff"});
 }
 else{
