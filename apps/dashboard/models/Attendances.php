fda4846f33271deef72d81e7d78827ab6c56096a
zinmonthet <zinmonthet@myanmar.gnext.asia>
2015-11-23 16:29:52 +0630

    Repair Manage user

--- apps/dashboard/models/Attendances.php
+++ apps/dashboard/models/Attendances.php
@@ -101,7 +101,7 @@ class Attendances extends Model {
         //select where user most leave taken
         /* $query    ="select member_login_name from core_member where member_id in
           (select member_id from absent group by member_id order by count(*) DESC) limit 3"; */
-        $query = "select member_login_name from core_member "
+        $query = "select * from core_member "
                 . "as c join absent as a on c.member_id=a.member_id "
                 . "where a.deleted_flag=0 group by a.member_id "
                 . "order by count(*) desc limit 3";
