6b8cffd4bed94444f4e1f1fb3a97bb1656625daa
SawZinMinTun <>
2015-12-09 08:53:54 +0630

    Merge origin/develop into develop

--- public/apps/leavedays/js/leavelist.js
+++ public/apps/leavedays/js/leavelist.js
@@ -23,7 +23,7 @@ Leave.init =  function(reload){
                 var json_obj = $.parseJSON(data);
                 for (var i in json_obj){
                    // alert(json_obj[i].full_name);
-                      dict.push(json_obj[i].full_name);
+                      dict.push(json_obj[i].member_login_name);
                       }                  
                 }
                         
