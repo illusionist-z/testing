213b60e18dd3556514b4e34054ec9205e76dd666
Yan Lin Pai <wizardrider@gmail.com>
2015-12-21 07:47:19 +0630

    #Change   	apps/attendancelist/controllers/IndexController.php 	apps/auth/controllers/LoginController.php 	apps/dashboard/controllers/IndexController.php 	apps/dashboard/views/index.phtml 	apps/dashboard/views/index/admin.phtml 	apps/dashboard/views/user/index.phtml 	apps/notification/controllers/IndexController.php 	config/config.ini 	library/core/controller.php

--- library/core/plugin/Permission.php
+++ library/core/plugin/Permission.php
@@ -1,5 +1,5 @@
 <?php namespace Library\Core\Plugin;
-
+ob_start();
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -69,3 +69,4 @@ class Permission extends Plugin
         return 'X';
     }
 }
+ob_end();
