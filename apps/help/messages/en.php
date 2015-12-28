e79e74cbc9c1371194db71376e7c4911289ec440
davijp <davidjawphan@gmail.com>
2015-12-28 16:58:07 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/attendancelist/controllers/IndexController.php
    	apps/attendancelist/views/index.phtml
    	apps/calendar/controllers/IndexController.php
    	apps/calendar/models/Calendar.php
    	apps/dashboard/controllers/IndexController.php
    	apps/dashboard/views/index.phtml
    	apps/dashboard/views/index/user.phtml
    	apps/leavedays/controllers/IndexController.php
    	apps/manageuser/controllers/IndexController.php
    	apps/salary/controllers/IndexController.php
    	apps/setting/controllers/IndexController.php
    	apps/setting/controllers/UserController.php
    	apps/setting/views/index.phtml
    	config/config.ini
    	library/core/controller.php
    	public/apps/attendancelist/js/index.js

--- apps/help/messages/en.php
+++ apps/help/messages/en.php
@@ -1,6 +1,24 @@
 <?php
 $messages = array(
     
-    "Search Help" =>"Search Help"
-   
+    "Search Help" =>"Search Help",
+   /**
+     * start for dashboard help center
+     */
+    "Dashboard_hc"=>"Dashboard help center",
+    "Manage User"=>"Manage User",
+    "Click Manage Users"=>"Click Manage Users Link Icon,You can Manage,edit and modify User Lists.",
+    "Attendance List"=>"Attendance List",
+    "Click Attendance Link"=>"Click Attendance Link Icon, You can review your Today and Monthly Attendance Lists.",
+    "Leave Days"=>"Leave Days",
+    "Click Leave Days Link"=>"Click Leave Days Link Icon, You can do your Apply Leave Day and can review your Leave Lists and Admin can  change Leave Setting.",
+    "Salary(Only For Admin)"=>"Salary(Only For Admin)",
+    "Click Salary Link"=>"Click Salary Link Icon,Admin can calculate salary of all SALTS members.",
+    "If you late"=>"If you late,you must write your late reason by click checkbox.",
+    "Check In Button"=>"Check In Button",
+    "It is describe"=>"It is describe as a your attending office today,so you must check in at the time you arrive at office.",
+    "Check Out Button"=>"Check Out Button",
+    "It is describe as"=>"It is describe as a your finished woring time today,so you must check out before you leave the office.",
+    "Status of"=>"Status of most not leave taken member and takes leave member.",
+    "Showing"=>"Showing the last registered member names"
 );
