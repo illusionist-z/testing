4dda861a9270a24f7f2f6796a5b835e4e8731317
SawZinMinTun <>
2015-12-28 16:26:04 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/attendancelist/views/index.phtml
    	apps/auth/controllers/LoginController.php
    	apps/calendar/views/index.phtml
    	apps/dashboard/controllers/IndexController.php
    	apps/help/controllers/IndexController.php
    	apps/help/views/index.phtml

--- apps/help/messages/ja.php
+++ apps/help/messages/ja.php
@@ -1,6 +1,29 @@
 <?php
 $messages = array(
+    /**
+     * Left side menu bar
+     */
+    "Search Help" =>"検索ヘルプ",
+    "Dashboard"=>"ホーム",
+    "Attendance List"=>"参加者リスト",
+    "Today Attendance List"=>"今日 Lists",
+    "Monthly Attendance List"=>"毎月Lists",
+    "Manage User"=>"管理ユーザー",
+    "Leave Days"=>"休み",
+    "Apply Leave"=>"休暇申請",
+    "Leave Lists"=>"休暇リスト",
+    "Leave Setting"=>"休暇設定",
+    "Calendar"=>"カレンダー",
+    "Salary"=>"給料",
+    "Add Salary"=>"給料新規入力",
+    "Salary Lists"=>"給料一覧",
+    "Monthly Salary Lists"=>"支払った給料リスト",
+    "Salary Setting"=>"給料設定",
+    "Allowance"=>"手当",
+    "Document"=>"ドキュメント",
+    "Letter Head"=>"レターヘッド",
+    "SSB & Tax Document"=>"SSB & 税金用紙",
     
-    "Search Help" =>"Janpan Search Help"
+    
    
 );
