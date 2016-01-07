0d7d5c3ce5971cd0d2ef37e38b0a45bd2fe3966f
zinmonthet <zinmonthet@myanmar.gnext.asia>
2016-01-07 10:52:08 +0630

    Merge origin/develop into develop
    Conflicts:
    	public/common/css/common.css

---
+++ apps/managecompany/messages/en.php
@@ -0,0 +1,20 @@
+<?php
+$messages = array(
+    /**
+     * Left side menu bar
+     */
+    "com_list" =>"Company List",    
+    "addcom" => " Add New Company",
+"company_id" =>"company ID",
+"company_name" => "Company Name",
+ "searchcname"=>"Search Company Name",
+"sdate" =>"Starting Date",
+"email" =>"email",
+ "pno" =>"phone num",
+"db_name" =>"Database Name",
+"uname" =>"DB UserName",
+"db_psw" =>" DB Password",
+"host" =>"Host Name",
+"user_limit"=>"User Limit",
+"module_flag"=>"Module Flag",
+    );
