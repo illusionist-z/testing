681467bd63503aa0628542cfcbf24e7fbfcb93bc
E5-471G <E5-471G@SUZINKYAW>
2016-01-11 11:29:47 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/auth/controllers/IndexController.php
    	apps/auth/controllers/LoginController.php
    	apps/managecompany/controllers/ControllerBase.php
    	apps/managecompany/controllers/IndexController.php
    	apps/managecompany/messages/en.php
    	apps/managecompany/messages/ja.php
    	apps/managecompany/views/index.phtml
    	apps/managecompany/views/index/addcompany.phtml
    	apps/managecompany/views/index/editcompany.phtml
    	apps/managecompany/views/index/index.phtml
    	apps/manageuser/views/index.phtml
    	config/config.ini
    	config/services.php

--- apps/managecompany/controllers/IndexController.php
+++ apps/managecompany/controllers/IndexController.php
@@ -13,33 +13,45 @@ class IndexController extends ControllerBase {
         parent::initialize();
         $this->setCommonJsAndCss();
         $this->view->t = $this->_getTranslation();
-      $this->assets->addJs('apps/managecompany/js/multiple.js');
-    $this->assets->addCss('common/css/css/style.css');
-   $this->assets->addCss('common/css/dialog.css');
+        $this->assets->addJs('apps/managecompany/js/multiple.js');
+        $this->assets->addCss('common/css/css/style.css');
+        $this->assets->addCss('common/css/dialog.css');
         
     }
-    
+    /**
+     * show manage company page
+     * @author Su Zin Kyaw <gnext.suzin@gmail.com>
+     */
     public function indexAction(){
        $company=new \salts\Managecompany\Models\CompanyTbl();
-     $result=  $company->getallcom();
+       $result=  $company->getallcom();
        $this->view->result=$result;
        
 
     }
-    
+    /**
+     * show addcompany page
+     * @author Su Zin Kyaw <gnext.suzin@gmail.com>
+     */
     public function addcompanyAction(){
         
     }
-    
+    /**
+     * show company detail to edit
+     * @author Su Zin Kyaw <gnext.suzin@gmail.com>
+     */
     public function editcompanyAction(){
-         $id=$this->request->get('id');
-         $company=new \salts\Managecompany\Models\CompanyTbl();
+        $id=$this->request->get('id');
+        $company=new \salts\Managecompany\Models\CompanyTbl();
         $result=$company->findDatabyId($id);
         $module=$company->findModulebyId($id);
-       $this->view->module=$module;
+        $this->view->module=$module;
         $this->view->result=$result;
     }
-    
+    /**
+     * @author Su Zin Kyaw <gnext.suzin@gmail.com>
+     * Add new company 
+     */
     public function addnewAction(){
         $com=$this->request->get('com');
         $check=$this->request->get('check');
@@ -48,11 +60,13 @@ class IndexController extends ControllerBase {
         $this->response->redirect("managecompany/index");
 
     }
-    
+    /**
+     * @author Su Zin Kyaw <gnext.suzin@gmail.com>
+     * Update Company Detail with new data
+     */
     public function updatecompanyAction(){
-          $com=$this->request->get('com');
-          $check=$this->request->get('check');
-          
+        $com=$this->request->get('com');
+        $check=$this->request->get('check');
         $company=new \salts\Managecompany\Models\CompanyTbl();
         $company->updatecom($com,$check);
         $this->response->redirect("managecompany/index");
--- apps/managecompany/models/CompanyTbl.php
+++ apps/managecompany/models/CompanyTbl.php
@@ -4,84 +4,77 @@ namespace salts\Managecompany\Models;
 use Phalcon\Mvc\Model;
 
 class CompanyTbl extends \Library\Core\BaseModel {
+
     public function initialize() {
         //parent::initialize();
         $this->db = $this->getDI()->getShared("db");
     }
-     
-    public function addnew($data,$check){
-       
-        $date=date("Y-m-d H:i:s");
-        $sql="INSERT INTO `company_tbl`(`company_id`, `company_name`, `email`, `phone_no`, "
+    /**
+     * 
+     * @param type $data
+     * @param type $check
+     * @author Su Zin Kyaw <gnext.suzin@gmail.com>
+     * Add New Company to company_tbl table
+     */
+    public function addnew($data, $check) {
+
+        $date = date("Y-m-d H:i:s");
+        $sql = "INSERT INTO `company_tbl`(`company_id`, `company_name`, `email`, `phone_no`, "
                 . "`db_name`, `user_name`, `db_psw`, `host`, `user_limit`, `starting_date`, "
                 . "`created_dt`) VALUES "
                 . "('" . $data['id'] . "','" . $data['name'] . "','" . $data['email'] . "','" . $data['phno'] . "'"
                 . ",'" . $data['db'] . "','" . $data['dbun'] . "','" . $data['dbpsw'] . "','" . $data['host'] . "',"
                 . "'" . $data['limit'] . "','" . $data['sdate'] . "','" . $date . "')";
-        $this->db->query( $sql);
-        for($i=0;$i<count($check);$i++)
-            {
+        $this->db->query($sql);
+        for ($i = 0; $i < count($check); $i++) {
             $sql = "INSERT INTO `enable_module`(`company_id`, `module_id`) VALUES ('" . $data['id'] . "','" . $check[$i] . "')";
-             $this->db->query($sql);   
-            }
-    }
-    
-    public function getallcom(){
-          $result=$this->db->query( "select * from company_tbl ");
-          
-        $final_result=$result->fetchall();
-        $i=0;
-        foreach ($final_result as $value) {
-          $result=$this->db->query( "select module_id from enable_module where company_id='".$value['company_id']."' ");  
-         $module=$result->fetchall();
-   $final_result[$i]['module']="aa";
-   $i++;
+            $this->db->query($sql);
         }
-//  var_dump($final_result);exit;
-        return $final_result;
     }
-    
-    public function findDatabyId($id){
-            $result=$this->db->query( "select * from company_tbl where company_tbl.company_id='".$id."' ");
-          
-        $final_result=$result->fetchArray();
-//        $i=0;
+
+    public function getallcom() {
+        $result = $this->db->query("select * from company_tbl ");
+
+        $final_result = $result->fetchall();
+        $i = 0;
 //        foreach ($final_result as $value) {
-//          $result=$this->db->query( "select module_id from enable_module where company_id='".$value['company_id']."' ");  
-//         
-//          $module=$result->fetchall();
-//     
-//   $final_result[$i]['module']="aa";
-//   $i++;
+//            $result = $this->db->query("select module_id from enable_module where company_id='" . $value['company_id'] . "' ");
+//            $module = $result->fetchall();
+//            $final_result[$i]['module'] = "aa";
+//            $i++;
 //        }
-//  var_dump($final_result);exit;
         return $final_result;
     }
-    public function findModulebyId($id){
-          $result=$this->db->query( "select module_id from enable_module where enable_module.company_id='".$id."' ");
-          
-        $final_result=$result->fetchAll();
+
+    public function findDatabyId($id) {
+        $result = $this->db->query("select * from company_tbl where company_tbl.company_id='" . $id . "' ");
+
+        $final_result = $result->fetchArray();
         return $final_result;
     }
-    public function updatecom($com,$check){ 
-          $date=date("Y-m-d H:i:s");
-         $sdate=date("Y-m-d",strtotime($com['sdate']));
-         
-    $sql="UPDATE company_tbl set company_name= '".$com['name']."' , email= '".$com['email']."',"
-            . "phone_no= '".$com['phno']."',db_name= '".$com['db']."' ,user_name= '".$com['dbun']."',"
-            . " db_psw= '".$com['dbpsw']."',host= '".$com['host']."',"
-            . "user_limit= '".$com['limit']."',starting_date= '".$sdate."',"
-            . "updated_dt= '".$date."' where company_tbl.company_id= '".$com['id']."'  ";
-        $this->db->query( $sql);
-        $this->db->query( "delete  from enable_module where enable_module.company_id='".$com['id']."' ");
-          for($i=0;$i<count($check);$i++)
-            {
+
+    public function findModulebyId($id) {
+        $result = $this->db->query("select module_id from enable_module where enable_module.company_id='" . $id . "' ");
+
+        $final_result = $result->fetchAll();
+        return $final_result;
+    }
+
+    public function updatecom($com, $check) {
+        $date = date("Y-m-d H:i:s");
+        $sdate = date("Y-m-d", strtotime($com['sdate']));
+
+        $sql = "UPDATE company_tbl set company_name= '" . $com['name'] . "' , email= '" . $com['email'] . "',"
+                . "phone_no= '" . $com['phno'] . "',db_name= '" . $com['db'] . "' ,user_name= '" . $com['dbun'] . "',"
+                . " db_psw= '" . $com['dbpsw'] . "',host= '" . $com['host'] . "',"
+                . "user_limit= '" . $com['limit'] . "',starting_date= '" . $sdate . "',"
+                . "updated_dt= '" . $date . "' where company_tbl.company_id= '" . $com['id'] . "'  ";
+        $this->db->query($sql);
+        $this->db->query("delete  from enable_module where enable_module.company_id='" . $com['id'] . "' ");
+        for ($i = 0; $i < count($check); $i++) {
             $sql = "INSERT INTO `enable_module`(`company_id`, `module_id`) VALUES ('" . $com['id'] . "','" . $check[$i] . "')";
-             $this->db->query($sql);   
-            }
-
+            $this->db->query($sql);
+        }
     }
-   
-    
-}
 
+}
