e2525653bce5186a9e386891575df315ed48a623
E5-471G <E5-471G@SUZINKYAW>
2016-01-18 13:26:42 +0630

    salary list edit repair

--- apps/auth/controllers/LoginController.php
+++ apps/auth/controllers/LoginController.php
@@ -22,11 +22,11 @@ class LoginController extends ControllerBase {
         $dbinfo['host']='localhost';
         $dbinfo['db_name']='company_db';
         $dbinfo['user_name']='root';
-        $dbinfo['db_psw']='root';
+        $dbinfo['db_psw']='';
         
         $this->session->set('db_config',$dbinfo);
         $result = $ModelAuth->check($loginParams, $user);
-        
+        $this->session->set('user', $result);
         // Data Base Chack
         if ($result) {
             $this->response->redirect('managecompany');
