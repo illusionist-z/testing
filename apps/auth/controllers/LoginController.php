b874b95beabd3e2279ee9233b6a057c81ffe2bbf
SawZinMinTun <>
2016-02-09 15:50:56 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/auth/controllers/IndexController.php

--- apps/auth/controllers/LoginController.php
+++ apps/auth/controllers/LoginController.php
@@ -62,7 +62,7 @@ class LoginController extends ControllerBase {
 
                 $result = $ModelAuth->check($login_params, $user);
                 $permission = $ModelAuth->getPermit($login_params);
-                //$Member = new CoreMember();
+                $Member = new CoreMember();
                 $ll = $Member::getInstance();
                 $lang = $Member->getLang($login_params);
                 $this->session->set('language', $lang['lang']);
@@ -78,7 +78,7 @@ class LoginController extends ControllerBase {
                 $this->session->set('tokenpush', $member_id);
                 
                 $member_name = $this->session->tokenpush;
-              //  $chack_user2 = new CoreMember();
+                $chack_user2 = new CoreMember();
                 $chack_user2 = $Member::findByMemberLoginName($member_name);
                 if (0 != count($chack_user2)) {
                     
