5340e94140bee0724417491a5968e64932fda7ac
SawZinMinTun <>
2016-02-09 15:25:58 +0630

    Merge origin/develop into develop
    Conflicts:
    	apps/auth/controllers/IndexController.php

--- apps/auth/models/CoreMember.php
+++ apps/auth/models/CoreMember.php
@@ -9,7 +9,28 @@ use Phalcon\Mvc\Model;
  */
 class CoreMember extends Model {
 
-    public $member_mail;
+    public $member_id;
+    public $user_rule_member_id;
     public $member_login_name;
+    public $full_name;
+    public $member_dept_name;
+    public $member_mobile_tel;
+    public $member_mail;
+    public $position;
+    public $user_rule;
+    public $lang;
+    public $member_address;
+    public $member_profille;
+    public $rank_code;
+    public $member_is_change;
+    public $working_start_dt;
+    public $working_year_by_year;
+    public $rs_status;
+    public $timeflag;
+    public $creator_id;
+    public $created_dt;
+    public $updater_id;
+    public $updated_dt;
+    public $deleted_flag;
 
 }
