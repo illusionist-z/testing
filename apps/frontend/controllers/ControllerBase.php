89db6b7973fe3194fbfaa52fa06059da3b161f60
Kohei Iwasa <copei@172.20.10.3>
2016-02-09 09:45:55 +0630

    Add front end controller.

---
+++ apps/frontend/controllers/ControllerBase.php
@@ -0,0 +1,24 @@
+<?php namespace 
+
+salts\Frontend\Controllers;
+
+class ControllerBase extends \Library\Core\Controller
+{
+    /**
+     * temp 
+     * @var type 
+     */
+    
+    public function initialize() {
+//        $this->session->set('index', 3);
+        if(null !== $this->session->get('index')){
+            $session = $this->session->get('index');
+        }else{
+            $session = 'B';
+        }
+        
+        echo $this->session->get('index');
+//       $session $this->session->set('index',3);
+        
+    }
+}
