5193938874df43a827f9ee9258ccffec2b63a6c6
zinmon <zinmonthet@myanmar.gnext.asia>
2016-05-02 14:30:55 +0630

    fix document module

--- apps/document/models/Document.php
+++ apps/document/models/Document.php
@@ -26,12 +26,11 @@ class Document extends Model {
                         "limit" => 1
             ));
             if (isset($salary)) {
-                $data = explode("-", $salary[0]->pay_date);
                 $row = $this->modelsManager->createBuilder()
                         ->columns(array('core.*', 'salary_detail.*'))
                         ->from(array('core' => 'salts\Core\Models\Db\CoreMember'))
                         ->join('salts\Document\Models\SalaryDetail', 'core.member_id = salary_detail.member_id', 'salary_detail')
-                        ->where('MONTH(salary_detail.pay_date) = :month: ', array('month' => $data[1]))
+                        ->where('salary_detail.pay_date = :pday: ', array('pday' => $salary[0]->pay_date))
                         ->andWhere('core.deleted_flag = 0')
                         ->andWhere('salary_detail.income_tax!=0 ')
                         ->getQuery()
@@ -55,14 +54,11 @@ class Document extends Model {
         try {
             $row = Array();
             if (isset($salary)) {
-                $data = explode("-", $salary[0]->pay_date);
-
-
                 $row = $this->modelsManager->createBuilder()
                         ->columns(array('core.*', 'salary_detail.*'))
                         ->from(array('core' => 'salts\Core\Models\Db\CoreMember'))
                         ->join('salts\Document\Models\SalaryDetail', 'core.member_id = salary_detail.member_id', 'salary_detail')
-                        ->where('MONTH(salary_detail.pay_date) = :month: ', array('month' => $data[1]))
+                        ->where('salary_detail.pay_date = :pday: ', array('pday' => $salary[0]->pay_date))
                         ->andWhere('core.deleted_flag = 0')
                         ->getQuery()
                         ->execute();
