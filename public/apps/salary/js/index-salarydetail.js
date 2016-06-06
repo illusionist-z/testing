a158f89e813ac7cf2c812e5b8dc5671d562cae6d
Su Zin Kyaw <gnext.suzin@gmail.com>
2016-06-06 08:25:58 +0630

    #Repair

--- public/apps/salary/js/index-salarydetail.js
+++ public/apps/salary/js/index-salarydetail.js
@@ -1,37 +1,7 @@
 // selects all the divs of class='sample',hides them, finds the first, and shows it
 $('div.sample').hide().first().show();
 var i=0;
-// binds a click event-handler to a elements whose class='display'
-$('a.display').on('click', function(e) {
-    // prevents the default action of the link
-    e.preventDefault();
-    
 
-    // assigns the currently visible div.sample element to a variable
-    var that = $('div.sample:visible'),
-        // assigns the text of the clicked-link to a variable for comparison purposes
-        t = $(this).text();
-        //alert(that.next('div.sample').length);
-    // checks if it was the 'next' link, and ensures there's a div to show after the currently-shown one
-    if (t == 'NEXT' && that.next('div.sample').length > 0) {
-        // hides all the div.sample elements
-        i++;
-        $('div.sample').hide();
-        
-        // shows the 'next'
-        
-        that.next('div.sample').show();
-    }
-    
-    // exactly the same as above, but checking that it's the 'prev' link
-    // and that there's a div 'before' the currently-shown element.
-    else if (t == 'PREV' && that.prev('div.sample').length > 0) {
-    i--;
-       
-        $('div.sample').hide();
-        that.hide().prev('div.sample').show();
-    }
-});
 //var Resign = {
 //    
 //      Add : function (id){
