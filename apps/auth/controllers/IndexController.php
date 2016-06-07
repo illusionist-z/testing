fe591df8e5ab9500fb2a81584e9a7b82e3f37fd5
SAWZINMINTUN <gnext.suzin@gmail.com>
2016-06-07 16:15:57 +0630

    change failer error for manage company

--- apps/auth/controllers/IndexController.php
+++ apps/auth/controllers/IndexController.php
@@ -46,7 +46,7 @@ class IndexController extends ControllerBase {
     public function failerAction($mode = 1) {
         /*  User failerAction  @author Yan Lin Pai <wizardrider@gmail.com> */
         $this->view->errorMsg = 'company id or user name or password wrong';
-        $this->view->errorMsgForgot = 'I forgot my password ?';
+//        $this->view->errorMsgForgot = 'I forgot my password ?';
         if (!isset($_SESSION["attempts"]))
             $_SESSION["attempts"] = 0;
      
@@ -184,6 +184,7 @@ class IndexController extends ControllerBase {
     /**  When user failed  email  go  @param type $mode  */
     public function failersuperuserAction() {
         $this->view->errorMsg = 'user name or password wrong';
+        $this->view->errorMsgForgot = '';
         $this->view->mode = 1;
         $this->view->pick('index/index');
     }
